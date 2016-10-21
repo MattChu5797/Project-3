ff5ad232f998afd40406ff2843d4ef4433402206
Matthew Chu <MattChu@Matthews-MBP-3.wireless.newschool.edu>
2016-10-21 18:11:34 -0400

    Project 3

---
+++ scripts.js
@@ -0,0 +1,553 @@
+// this line of code sets up your canvas
+var r = new Rune({
+  container: "body",
+  width: '100%',
+  height: '100%'
+});
+// write your own code below this line
+//code3
+
+
+r. triangle(0, 0, 100, 0, 0,400)
+  .fill('#ffff00');
+r. triangle(100, 0, 200, 0, 100,400)
+  .fill('#0000ff');
+r. triangle(200, 0, 300, 0, 200,400)
+  .fill('#ff0000');
+r. triangle(300, 0, 400, 0, 300,400)
+  .fill('#ffff00');
+r. triangle(400, 0, 500, 0, 400,400)
+  .fill('#0000ff');
+r. triangle(500, 0, 600, 0, 500,400)
+    .fill('#ff0000');
+r. triangle(600, 0, 700, 0, 600,400)
+    .fill('#ffff00');
+r. triangle(700, 0, 800, 0, 700,400)
+    .fill('#0000ff');
+r. triangle(800, 0, 900, 0, 800,400)
+      .fill('#ff0000');
+r. triangle(900, 0, 1000, 0, 900,400)
+      .fill('#ffff00');
+r. triangle(1000, 0, 1100, 0, 1000,400)
+      .fill('#0000ff');
+r. triangle(1100, 0, 1200, 0, 1100,400)
+        .fill('#ff0000');
+r. triangle(1200, 0, 1300, 0, 1200,400)
+        .fill('#ffff00');
+r. triangle(1300, 0, 1400, 0, 1300,400)
+        .fill('#0000ff');
+r. triangle(1400, 0, 1500, 0, 1400,400)
+          .fill('#ff0000');
+r. triangle(1500, 0, 1600, 0, 1500,400)
+          .fill('#ffff00');
+r. triangle(1600, 0, 1700, 0, 1600,400)
+          .fill('#0000ff');
+r. triangle(1700, 0, 1800, 0, 1700,400)
+            .fill('#ff0000');
+r. triangle(1800, 0, 1900, 0, 1800,400)
+            .fill('#ffff00');
+r. triangle(1900, 0, 2000, 0, 1900,400)
+            .fill('#0000ff');
+    r. triangle(0, 400, 100, 0, 100,400)
+            .fill('#00ff00');
+r. triangle(200, 400, 200, 0, 100,400)
+            .fill('#ff00ff');
+r. triangle(300, 400, 300, 0, 200,400)
+            .fill('#ff6600');
+r. triangle(400, 400, 400, 0, 300,400)
+            .fill('#00ff00');
+r. triangle(500, 400, 500, 0, 400,400)
+            .fill('#ff00ff');
+r. triangle(600, 400, 600, 0, 500,400)
+            .fill('#ff6600');
+r. triangle(700, 400, 700, 0, 600,400)
+            .fill('#00ff00');
+r. triangle(800, 400, 800, 0, 700,400)
+            .fill('#ff00ff');
+r. triangle(900, 400, 900, 0, 800,400)
+            .fill('#ff6600');
+r. triangle(1000, 400,1000, 0, 900,400)
+            .fill('#00ff00');
+r. triangle(1100, 400, 1100, 0, 1000,400)
+            .fill('#ff00ff');
+r. triangle(1200, 400, 1200, 0, 1100,400)
+            .fill('#ff6600');
+r. triangle(1300, 400,1300, 0, 1200,400)
+            .fill('#00ff00');
+r. triangle(1400, 400, 1400, 0, 1300,400)
+            .fill('#ff00ff');
+r. triangle(1500, 400, 1500, 0, 1400,400)
+            .fill('#ff6600');
+r. triangle(0, 750, 100, 400, 0,400)
+            .fill('#ffff00');
+r. triangle(100, 750, 200, 400, 100,400)
+            .fill('#0000ff');
+r. triangle(200, 750, 300, 400, 200,400)
+            .fill('#ff0000');
+r. triangle(300,750, 400, 400, 300,400)
+            .fill('#ffff00');
+r. triangle(400, 750, 500, 400, 400,400)
+                          .fill('#0000ff');
+r. triangle(500, 750, 600, 400, 500,400)
+                          .fill('#ff0000');
+r. triangle(600, 750, 700, 400, 600,400)
+                          .fill('#ffff00');
+r. triangle(700, 750, 800, 400, 700,400)
+                          .fill('#0000ff');
+r. triangle(800, 750, 900, 400, 800,400)
+                          .fill('#ff0000');
+r. triangle(900, 750, 1000, 400, 900,400)
+                          .fill('#ffff00');
+r. triangle(1000, 750, 1100, 400, 1000,400)
+                          .fill('#0000ff');
+r. triangle(1100, 750, 1200,400, 1100,400)
+                          .fill('#ff0000');
+r. triangle(1200, 750, 1300,400, 1200,400)
+                          .fill('#ffff00');
+r. triangle(1300, 750, 1400,400, 1300,400)
+                          .fill('#0000ff');
+r. triangle(1400, 750,1500, 400, 1400,400)
+                          .fill('#ff0000');
+r. triangle(1500, 750,1600,400, 1600,400)
+                          .fill('#ffff00');
+r. triangle(0, 750, 100, 400, 100,750)
+              .fill('#00ff00');
+r. triangle(100, 750, 200, 400, 200,750)
+              .fill('#ff00ff');
+r. triangle(200, 750, 300, 400, 300,750)
+              .fill('#ff6600');
+r. triangle(300, 750, 400, 400, 400,750)
+              .fill('#00ff00');
+r. triangle(400, 750, 500, 400, 500,750)
+              .fill('#ff00ff');
+r. triangle(500, 750, 600, 400, 600,750)
+              .fill('#ff6600');
+r. triangle(600, 750, 700, 400, 700,750)
+              .fill('#00ff00');
+r. triangle(700, 750, 800, 400, 800,750)
+              .fill('#ff00ff');
+r. triangle(800, 750, 900, 400, 900,750)
+              .fill('#ff6600');
+r. triangle(900, 750, 1000, 400, 1000,750)
+              .fill('#00ff00');
+r. triangle(1000, 750, 1100, 400, 1100,750)
+              .fill('#ff00ff');
+r. triangle(1100, 750, 1200, 400, 1200,750)
+              .fill('#ff6600');
+r. triangle(1200, 750, 1300, 400, 1300,750)
+              .fill('#00ff00');
+r. triangle(1300, 750, 1400, 400, 1400,750)
+              .fill('#ff00ff');
+r. triangle(1400, 750, 1500, 400, 1500,750)
+              .fill('#ff6600');
+var ellipsea = r.ellipse(700,50,50,50)
+              .fill('#ffffff');
+                ellipsea.move(100,0);
+              r.on('update', function() {
+                ellipsea.move (0,1,true);
+              });
+              r.play()
+var ellipseb = r.ellipse(700,50,50,50)
+                .fill('#ffffff');
+                ellipseb.move(200,900);
+              r.on('update', function() {
+                ellipseb.move (0,-1,true);
+              });
+              r.play()
+var ellipsec = r.ellipse(700,50,50,50)
+                          .fill('#ffffff');
+                            ellipsec.move(300,0);
+                          r.on('update', function() {
+                            ellipsec.move (0,1,true);
+                          });
+                          r.play()
+var ellipsed = r.ellipse(700,50,50,50)
+                            .fill('#ffffff');
+                            ellipsed.move(400,900);
+                          r.on('update', function() {
+                            ellipsed.move (0,-1,true);
+                          });
+                          r.play()
+var ellipsee = r.ellipse(700,50,50,50)
+                                      .fill('#ffffff');
+                                        ellipsee.move(500,0);
+                                      r.on('update', function() {
+                                        ellipsee.move (0,1,true);
+                                      });
+                                      r.play()
+var ellipsef = r.ellipse(700,50,50,50)
+                                        .fill('#ffffff');
+                                        ellipsef.move(600,900);
+                                      r.on('update', function() {
+                                        ellipsef.move (0,-1,true);
+                                      });
+                                      r.play()
+var ellipseg = r.ellipse(700,50,50,50)
+                                  .fill('#ffffff');
+                              ellipseg.move(700,0);
+                                    r.on('update', function() {
+                        ellipseg.move (0,1,true);
+                                    });
+                                    r.play()
+var ellipseh = r.ellipse(700,50,50,50)
+                        .fill('#ffffff');
+                          ellipseh.move(800,900);
+                      r.on('update', function() {
+                      ellipseh.move (0,-1,true);
+                            });
+                        r.play()
+var ellipsei = r.ellipse(700,50,50,50)
+             .fill('#ffffff');
+                  ellipsei.move(900,0);
+                    r.on('update', function() {
+                      ellipsei.move (0,1,true);
+                        });
+                        r.play()
+var ellipsej = r.ellipse(700,50,50,50)
+                        .fill('#ffffff');
+                    ellipsej.move(1000,900);
+                    r.on('update', function() {
+                      ellipsej.move (0,-1,true);
+                  });
+                                  r.play()
+var ellipsek = r.ellipse(700,50,50,50)
+                    .fill('#ffffff');
+                    ellipsek.move(1100,0);
+                    r.on('update', function() {
+                      ellipsek.move (0,1,true);
+                                        });
+                                  r.play()
+var ellipsel = r.ellipse(700,50,50,50)
+                    .fill('#ffffff');
+                    ellipsel.move(1200,900);
+                    r.on('update', function() {
+                      ellipsel.move (0,-1,true);
+                                    });
+                            r.play()
+var ellipsem = r.ellipse(700,50,50,50)
+                  .fill('#ffffff');
+                  ellipsem.move(1300,0);
+                    r.on('update', function() {
+                      ellipsem.move (0,1,true);
+                              });
+                            r.play()
+var ellipsen = r.ellipse(700,50,50,50)
+              .fill('#ffffff');
+              ellipsen.move(1400,900);
+              r.on('update', function() {
+              ellipsen.move (0,-1,true);
+                            });
+              r.play()
+var ellipseo = r.ellipse(700,50,50,50)
+            .fill('#ffffff');
+              ellipseo.move(0,400);
+              r.on('update', function() {
+              ellipseo.move (1,0,true);
+                              });
+                r.play()
+var ellipsep = r.ellipse(700,50,50,50)
+              .fill('#ffffff');
+                  ellipsep.move(1500,400);
+                  r.on('update', function() {
+              ellipsep.move (-1,0,true);
+                            });
+                r.play()
+
+
+//code2
+
+// r.rect(0,0,10000,10000)
+//   .fill ('#000000');
+// var ellipse1 = r.ellipse(50,50,50,50)
+//   .fill('#BED661');
+//
+// r.on('update', function() {
+//   ellipse1.move (0,.02,true);
+// });
+// r.play(10);
+//
+// var ellipse2 = r.ellipse(200,50,50,50)
+//   .fill('#999966');
+//
+// r.on('update', function() {
+//   ellipse2.move (0,.04,true);
+// });
+// r.play(10);
+//
+// var ellipse3 = r.ellipse(300,50,50,50)
+//   .fill('#336699');
+//
+// r.on('update', function() {
+//   ellipse3.move (0,.06,true);
+// });
+// r.play(10);
+//
+// var ellipse4 = r.ellipse(400,50,50,50)
+//   .fill('#666699');
+//
+// r.on('update', function() {
+//   ellipse4.move (0,.03,true);
+// });
+// r.play(10);
+//
+// var ellipse5 = r.ellipse(500,50,50,50)
+//   .fill('#669999');
+//
+// r.on('update', function() {
+//   ellipse5.move (0,.07,true);
+// });
+// r.play(10);
+//
+// var ellipse6 = r.ellipse(1600,50,50,50)
+//   .fill('#669999');
+//
+// r.on('update', function() {
+//   ellipse6.move (0,0.005,true);
+// });
+// r.play(10);
+//
+// var ellipse7 = r.ellipse(1700,50,50,50)
+//   .fill('#006666');
+//
+// r.on('update', function() {
+//   ellipse7.move (0,.02,true);
+// });
+// r.play(10);
+//
+// var ellipse8 = r.ellipse(800,50,50,50)
+//   .fill('#003399');
+//
+// r.on('update', function() {
+//   ellipse8.move (0,.04,true);
+// });
+// r.play(10);
+//
+// var ellipse9 = r.ellipse(900,50,50,50)
+//   .fill('#336699');
+//
+// r.on('update', function() {
+//   ellipse9.move (0,.06,true);
+// });
+// r.play(10);
+//
+// var ellipse10 = r.ellipse(1000,50,50,50)
+//   .fill('#BED661');
+//
+// r.on('update', function() {
+//   ellipse10.move (0,.03,true);
+// });
+// r.play(10);
+//
+// var ellipse11 = r.ellipse(1100,50,50,50)
+//   .fill('#660033');
+//
+// r.on('update', function() {
+//   ellipse11.move (0,.07,true);
+// });
+// r.play(10);
+//
+// var ellipse12 = r.ellipse(1500,50,50,50)
+//   .fill('#BED661');
+//
+// r.on('update', function() {
+//   ellipse12.move (0,.03,true);
+// });
+// r.play()
+// var ellipse13 = r.ellipse(1400,50,50,50)
+//   .fill('#660033');
+//   ellipse13.move(70,0);
+// r.on('update', function() {
+//   ellipse13.move (0,.01,true);
+// });
+// r.play()
+// var ellipse14 = r.ellipse(1300,50,50,50)
+//   .fill('#666633');
+//   ellipse14.move(1200,0);
+// r.on('update', function() {
+//   ellipse14.move (0,.05,true);
+// });
+// r.play()
+// var ellipse15 = r.ellipse(1200,50,50,50)
+//   .fill('#339966');
+//   ellipse15.move(0,0);
+// r.on('update', function() {
+//   ellipse15.move (0,.03,true);
+// });
+// var ellipse16 = r.ellipse(2000,50,50,50)
+//   .fill('#333300');
+//   ellipse16.move(700,0);
+// r.on('update', function() {
+//   ellipse16.move (0,.29,true);
+// });
+// var ellipse17 = r.ellipse(750,50,50,50)
+//   .fill('#993333');
+//   ellipse17.move(600,0);
+// r.on('update', function() {
+//   ellipse17.move (0,0.3,true);
+// });
+// var ellipse18 = r.ellipse(750,50,50,50)
+//   .fill('#333300');
+//   ellipse18.move(1300,0);
+// r.on('update', function() {
+//   ellipse18.move (0,0.3,true);
+// });
+
+
+
+// 3rdcode
+
+// r.rect(0,0,10000,10000)
+//   .fill('#ff0000')
+// var rectangle3 = r.rect(50,50,50,50)
+//   .fill('#ffff00')
+//   .move(0, 800);
+// r.on('update', function() {
+//   rectangle3.move (0.1,0,true);
+// });
+// r.play();
+//
+// var rectangle5 = r.rect(50,50,50,50)
+//   .fill('#ffffff')
+//   .move(1000, 0);
+//
+// r.on('update', function() {
+//   rectangle5.move (-0.1, 100,true);
+// });
+// r.play();
+//
+// var rectangle4 = r.rect(50,50,50,50)
+//   .fill('#000000')
+//   .move(0,1100 );
+// r.on('update', function() {
+//   rectangle4.move (0,-0.1,true);
+// });
+// r.play();
+//
+// var rectangle6 = r.rect(50,50,50,50)
+//   .fill('#ffff00')
+//   .move(100,1100 );
+// r.on('update', function() {
+//   rectangle6.move (0,-0.1,true);
+// });
+// r.play();
+//
+// var rectangle7 = r.rect(50,50,50,50)
+//   .fill('#ffffff')
+//   .move(200,1100 );
+// r.on('update', function() {
+//   rectangle7.move (0,-0.1,true);
+// });
+// r.play();
+//
+// var rectangle8 = r.rect(50,50,50,50)
+//   .fill('#000000')
+//   .move(150,0 );
+// r.on('update', function() {
+//   rectangle8.move (0,0.1,true);
+// });
+// r.play();
+//
+// var rectangle9 = r.rect(50,50,50,50)
+//   .fill('#ffff00')
+//   .move(50,0 );
+// r.on('update', function() {
+//   rectangle9.move (0,0.1,true);
+// });
+// r.play();
+//
+// var rectangle10 = r.rect(50,50,50,50)
+//   .fill('#ffffff')
+//   .move(250,0 );
+// r.on('update', function() {
+//   rectangle10.move (0,0.1,true);
+// });
+// r.play();
+//
+// var rectangle11 = r.rect(50,50,50,50)
+//   .fill('#000000')
+//   .move(350,0 );
+// r.on('update', function() {
+//   rectangle11.move (0,0.1,true);
+// });
+// r.play();
+//
+// var rectangle12 = r.rect(50,50,50,50)
+//   .fill('#ffff00')
+//   .move(400,1100 );
+// r.on('update', function() {
+//   rectangle12.move (0,-0.1,true);
+// });
+// r.play();
+//
+// var rectangle13 = r.rect(50,50,50,50)
+//   .fill('#ffffff')
+//   .move(0, 500);
+//
+// r.on('update', function() {
+//   rectangle13.move (0.1,0,true);
+// });
+// r.play();
+//
+// var rectangle14 = r.rect(50,50,50,50)
+//   .fill('#000000')
+//   .move(0, 1300);
+// r.on('update', function() {
+//   rectangle14.move (0.1,0,true);
+// });
+// r.play();
+// var rectangle15 = r.rect(50,50,50,50)
+//   .fill('#ffff00')
+//   .move(500,1100 );
+// r.on('update', function() {
+//   rectangle15.move (0,-0.1,true);
+// });
+// r.play();
+//
+// var rectangle16 = r.rect(50,50,50,50)
+//   .fill('#000000')
+//   .move(1100,0 );
+// r.on('update', function() {
+//   rectangle16.move (0,0.1,true);
+// });
+// r.play();
+//
+// var rectangle17 = r.rect(50,50,50,50)
+//   .fill('#ffff00')
+//   .move(800,0 );
+// r.on('update', function() {
+//   rectangle17.move (0,0.1,true);
+// });
+// r.play();
+//
+// var rectangle18 = r.rect(50,50,50,50)
+//   .fill('#ffffff')
+//   .move(600,0 );
+// r.on('update', function() {
+//   rectangle18.move (0,0.1,true);
+// });
+// r.play();
+// var rectangle19 = r.rect(50,50,50,50)
+//   .fill('#000000')
+//   .move(900,0 );
+// r.on('update', function() {
+//   rectangle19.move (0,0.1,true);
+// });
+// r.play();
+// var rectangle20 = r.rect(50,50,50,50)
+//   .fill('#ffffff')
+//   .move(1000,0 );
+// r.on('update', function() {
+//   rectangle20.move (0,0.1,true);
+// });
+// r.play();
+// var rectangle21 = r.rect(50,50,50,50)
+//   .fill('#ffffff')
+//   .move(50,400);
+//
+// r.on('update', function() {
+//   rectangle21.move (0.1,0,true);
+// });
+// r.play();
+// and above this line
+// this line of code draws your shapes
+r.draw();
